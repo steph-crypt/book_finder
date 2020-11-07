b5ac7b9d51622e79d19583200b745c3c037a1853
Sofia <sgandsteph@gmail.com>
2020-11-07 16:43:09 +0100

    creates search params component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 const SearchParams = () => {
+  const location = "San Francisco, CA";
+  return (
+    <div className="search params">
+      <form>
+        <label htmlFor="location">
+        location
+          <input id="location" value={location} placeholder="Location"/>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-}
+export default SearchParams;
